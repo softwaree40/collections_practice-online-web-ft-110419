f3f251042e6c03a73f29f62c8c435884a3cdc5cc
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 23:42:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-
+require 'pry'
+def sort_array_asc(array)
+    array.sort
+  
+end
+def sort_array_desc(array)
+   array.sort! {|x, y| y <=> x}
+end
+def sort_array_char_count(array)
+    array.sort_by { |word| word.length }
+  
+end
+def swap_elements(array)
+   array[2] = array[0],array[2],array[1]
+   
+end
+def reverse_array(array)
+   array.reverse
+end
+def kesha_maker(array)
+   
+   new_array = []
+   array.each do |word|
+     left = word[0..1]
+     right = word[3..-1]
+      new_array << left + '$' + right
+      
+      #binding.pry
+     
+   end
+   new_array
+    
+end
